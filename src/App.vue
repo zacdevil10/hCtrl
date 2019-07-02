96b84856adc1e3400cc586344f6c4ff0d7b8e36a
Zac Hadjineophytou <zac1999ad@gmail.com>
2019-07-02 01:18:12 +0100

    Created button widget

--- src/App.vue
+++ src/App.vue
@@ -2,23 +2,71 @@
   <div id="app">
     <Background/>
     <ClockWidget id="clock"/>
+
+    <div id="nav-bar">
+      <ImageButtonWidget icon="wb_sunny" :width='buttonWidth' :height='buttonHeight' :tint='buttonColor' />
+      <ImageButtonWidget icon="highlight" :width='buttonWidth' :height='buttonHeight' :tint='buttonColor' />
+      <ImageButtonWidget icon="highlight" :width='buttonWidth' :height='buttonHeight' :tint='buttonColor' />
+      <ImageButtonWidget icon="highlight" :width='buttonWidth' :height='buttonHeight' :tint='buttonColor' />
+      <ImageButtonWidget icon="highlight" :width='buttonWidth' :height='buttonHeight' :tint='buttonColor' />
+      <ImageButtonWidget icon="dashboard" :width='buttonWidth' :height='buttonHeight' :tint='buttonColor' />
+      <ImageButtonWidget icon="settings" :width='buttonWidth' :height='buttonHeight' :tint='buttonColor' />
+    </div>
   </div>
 </template>
 
 <script>
 import ClockWidget from './components/Clock.vue'
 import Background from './components/Background.vue'
+import ImageButtonWidget from './components/ImageButton.vue'
 
 export default {
   name: 'app',
   components: {
     Background,
-    ClockWidget
+    ClockWidget,
+    ImageButtonWidget
+  },
+  data() {
+    return {
+      buttonColor: 'rgba(128,128,128,0.5)',
+      buttonWidth: '150',
+      buttonHeight: '150'
+    }
   }
 }
 </script>
 
 <style>
+@font-face {
+  font-family: 'Material Icons';
+  font-style: normal;
+  font-weight: 400;
+  src: url(https://fonts.gstatic.com/s/materialicons/v47/flUhRq6tzZclQEJ-Vdg-IuiaDsNcIhQ8tQ.woff2) format('woff2');
+}
+
+.material-icons {
+  font-family: 'Material Icons';
+  font-weight: normal;
+  font-style: normal;
+  font-size: 120px;
+  line-height: 1;
+  letter-spacing: normal;
+  text-transform: none;
+  display: inline-block;
+  white-space: nowrap;
+  word-wrap: normal;
+  direction: ltr;
+  -webkit-font-feature-settings: 'liga';
+  -webkit-font-smoothing: antialiased;
+  color: white;
+}
+
+i.material-icons {
+  line-height: 150px;
+  vertical-align: bottom;
+}
+
 body {
   margin: 0;
 }
@@ -34,6 +82,14 @@ body {
 <style scoped>
 #clock {
   padding-top: 30vh;
+}
+
+#nav-bar {
+  width: 100%;
+  display: inline-flex;
+  position: fixed;
+  bottom: 0;
+  left: 0;
 }
 </style>
 
---
+++ src/components/ImageButton.vue
@@ -0,0 +1,34 @@
+<template>
+    <div id="icon-button" :style="{ width: this.$props.width + 'px',
+                                    height: this.$props.height + 'px',
+                                    backgroundColor: this.$props.tint}">
+        <i class="material-icons">{{ icon }}</i>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'ImageButtonWidget',
+    props: {
+        icon: String,
+        tint: String,
+        width: String,
+        height: String
+    }
+}
+</script>
+
+<style scoped>
+#icon-button {
+    margin: 20px auto;
+    opacity: 0.8;
+    background: inherit;
+    transition: all .15s ease-in-out;
+}
+
+#icon-button:hover {
+    opacity: 1;
+    transform: scale(1.1);
+    transition: all .15s ease-in-out;
+}
+</style>
